bc0f19d38cc043155ab0cca013ab0141f659f766
2hill <jamil.rahou@laposte.net>
2018-08-18 19:04:19 -0400

    animcss

--- src/components/Anim.js
+++ src/components/Anim.js
@@ -6,13 +6,13 @@ class Anim extends React.Component {
         return (
 <div>
            
-            <section className="star-wars">
+            <div className="star-wars">
 
                 <div className="crawl">
 
-                    <div className="title">
-                        <p>Episode X</p>
-                        <h1>Search the Wiki</h1>
+                    <div className="animtitle">
+                        <p>Episode XII</p>
+                        <h2>Search the Wiki</h2>
                     </div>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -21,7 +21,7 @@ class Anim extends React.Component {
 
                 </div>
 
-                </section>
+                </div>
                 </div>
         );
     }
