4f8e7cfc90938ddd9bb8762186b89ada67f7d3ed
2hill <jamil.rahou@laposte.net>
2018-08-18 18:46:22 -0400

    anim2

--- src/components/Anim.js
+++ src/components/Anim.js
@@ -4,11 +4,25 @@ import React from 'react';
 class Anim extends React.Component {
     render () {
         return (
-    <div className="starwars-demo">
-                <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" className="star"/>
-                    <img src="//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" className="wars"/>
-                        <h2 className="byline" id="byline">Pocket Wiki</h2>
-    </div>
+<div>
+           
+            <section className="star-wars">
+
+                <div className="crawl">
+
+                    <div className="title">
+                        <p>Episode X</p>
+                        <h1>Search the Wiki</h1>
+                    </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+                </div>
+
+                </section>
+                </div>
         );
     }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Results from './Results';
+//import Results from './Results';
 import Nav from './Nav';
 import Header from './Header';
 import Anim from './Anim';
