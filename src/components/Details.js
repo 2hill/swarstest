0c47144da706216a484ae1005ff73787df220f00
2hill <jamil.rahou@laposte.net>
2018-09-19 22:31:48 -0400

    megaFetch

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,12 +3,7 @@ import Header from './Header';
 import Results from './Results';
 import Nav from './Nav';
 import Search from './Search';
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> master
 class Details extends React.Component {
 
     constructor() {
@@ -16,119 +11,44 @@ class Details extends React.Component {
         this.state = {
             ships: [],
             planets: [],
-            characters: [],
-            loading: true
+            characters: []
         };
-        this.loadShips = this.loadShips.bind(this);
-        this.loadPlanets = this.loadPlanets.bind(this);
-        this.loadCharacters = this.loadCharacters.bind(this);
     }
 
     componentDidMount() {
-        /* fetch starships*/
-        let initialStarships = [];
-        fetch(`https://swapi.co/api/starships/`)
-            .then(response => response.json())
-            .then(data => {
-                initialStarships = data.results.map((ship) => {
-                    return ship
-                });
+ 
+        /* Mega Fetch */
 
-                this.setState({
-                    ships: initialStarships,
-                });
-            })
-            .catch(err => console.error(err));
-    
-        /* fetch planets*/
-        let initialPlanets = [];
-        fetch(`https://swapi.co/api/planets/`)
-            .then(response => response.json())
-            .then(data => {
-                initialPlanets = data.results.map((planet) => {
-                    return planet
-                });
-
-                this.setState({
-                    planets: initialPlanets,
-                    loading: false
-                });
-            })
-            .catch(err => console.error(err));
-
-        /* fetch characters */
         let initialCharacters = [];
-        fetch(`https://swapi.co/api/people/`)
+        let initialStarships = [];
+        let initialPlanets = [];
+        fetch(`https://swapi.co/api/people/` && `https://swapi.co/api/starships/` && `https://swapi.co/api/planets/`)
             .then(response => response.json())
             .then(data => {
                 initialCharacters = data.results.map((character) => {
                     return character
                 });
+                initialPlanets = data.results.map((planet) => {
+                    return planet
+                });
+                initialStarships = data.results.map((ship) => {
+                    return ship
+                });
+                console.log(initialStarships);
 
                 this.setState({
                     characters: initialCharacters,
-                    loading: false
+                    planets: initialPlanets,
+                    ships: initialStarships,
+                    
                 });
             })
             .catch(err => console.error(err));
-    }
-
-    componentDidUpdate(prevProps) {
-        const currentSearchTerm = this.props.match.params.searchTerm;
-        const oldSearchTerm = prevProps.match.params.searchTerm;
-        if (currentSearchTerm !== oldSearchTerm) {
-            this.loadShips(currentSearchTerm);
+  
         }
-    }
 
-
-    loadShips = (searchTerm = "ship") => {
-        this.setState({
-            loading: true
-        });
-        // Check for ships in local storage
-        const localStorageShips = localStorage.getItem(`search-${searchTerm}`);
-        if (localStorageShips) {
-            const localShips = JSON.parse(localStorageShips);
-            this.setState({
-                ships: localShips,
-                loading: false
-            });
-            return; // stop before fetch happens!
-        }
-    };
-
-    loadPlanets = (searchTerm = "planet") => {
-        this.setState({
-            loading: true
-        });
-        // Check for planets in local storage
-        const localStoragePlanets = localStorage.getItem(`search-${searchTerm}`);
-        if (localStoragePlanets) {
-            const localPlanets = JSON.parse(localStoragePlanets);
-            this.setState({
-                planets: localPlanets,
-                loading: false
-            });
-            return;
-        }
-    };
-
-    loadCharacters = (searchTerm = "character") => {
-        this.setState({
-            loading: true
-        });
-        // Check for ships in local storage
-        const localStorageCharacters = localStorage.getItem(`search-${searchTerm}`);
-        if (localStorageCharacters) {
-            const localCharacters = JSON.parse(localStorageCharacters);
-            this.setState({
-                characters: localCharacters,
-                loading: false
-            });
-            return;
-        }
-    };
+  
+    
 
     render (){
         return (
